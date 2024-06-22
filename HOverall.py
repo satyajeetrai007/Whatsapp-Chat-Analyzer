f75906fcb7081ffd2aab4635223e7c17a186031b
satyajeetrai007 <126376637+satyajeetrai007@users.noreply.github.com>
2024-06-23 01:24:39 +0530

    Update HOverall.py

--- HOverall.py
+++ HOverall.py
@@ -135,7 +135,7 @@ def omost_active_users(df):
     with col2:
         st.dataframe(new_df)    
 def oword_cloud(df,user):
-    f = open('.venv\stop_hinglish.txt','r',encoding='utf-8')
+    f = open('stop_hinglish.txt','r',encoding='utf-8')
     stop_words = f.read().split()
 
     temp = df[df['users'] != 'group_notification']
@@ -159,7 +159,7 @@ def oword_cloud(df,user):
 
 
 def omost_common_words(df):
-    f = open('.venv\stop_hinglish.txt','r',encoding='utf-8')
+    f = open('stop_hinglish.txt','r',encoding='utf-8')
     stop_words = f.read().split()
 
 
